4365ebe8d1acd43a21d4a094fc5966ad1b5fed43
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2024-01-10 20:32:10 +0300

    farany

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Headers from './components/Header';
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Headers></Headers>
+     <Card></Card>
+    </div>
+  );
+}
+
+export default App;
