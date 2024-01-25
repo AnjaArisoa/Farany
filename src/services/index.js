e9f17c5b0a335019ddc0aad4061e7b083b0eeebc
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2024-01-25 18:32:11 +0300

    services

---
+++ src/services/index.js
@@ -0,0 +1,12 @@
+import axiosInstance from "./axioxinstance";
+
+export async function annonces(){
+    if(localStorage.getItem("auth") != null){
+        let token = localStorage.getItem("auth");
+        return axiosInstance.get("https://rest-production-e2d3.up.railway.app/annonces", {
+            headers: {"Authorization" : `Bearer ${token}`}
+        });
+    }else{
+        return axiosInstance.get("https://rest-production-e2d3.up.railway.app/annonces");
+    }
+}
